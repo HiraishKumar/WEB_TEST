b7e28a8d6932f8ff8a6421aae8a7bcd0929e7d53
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-06-16 23:37:14 +0530

    added file based routing

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const {getalltasks,createtask,updatetask,deletetask,gettask} = require('../controllers/tasks')
 
+
+// what 
+
 router.route('/').get(getalltasks).post(createtask);
 router.route('/:id').get(gettask).patch(updatetask).delete(deletetask);
 
